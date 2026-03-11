eb7716174a95712f09726ee71ab89d91b7e5194d
Amangoua Ange <amangouakacouange@gmail.com>
2026-03-11 13:42:02 +0000

    remove gui

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,17 +6,8 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { TextGeometry } from "three/examples/jsm/Addons.js";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 
-// Environnment Map
-// const hdrLoader = new HDRLoader();
-// hdrLoader.load("./src/textures/environmentMap/2k.hdr", (environmentMap) => {
-//   environmentMap.mapping = THREE.EquirectangularReflectionMapping;
-
-//   scene.background = environmentMap;
-//   scene.environment = environmentMap;
-// });
-
 // GUI
-const gui = new GUI();
+// const gui = new GUI();
 // Canvas
 const canvas = document.querySelector(".webgl");
 
@@ -101,123 +92,6 @@ window.addEventListener("resize", () => {
   renderer.setSize(size.width, size.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
-// Creating textures
-// const textureLoader = new THREE.TextureLoader();
-// const doorTexture = textureLoader.load("./src/textures/door/color.jpg");
-// const matcapTexture = textureLoader.load("./src/textures/matcaps/1.png");
-// const doorHeightTexture = textureLoader.load("./src/textures/door/height.jpg");
-// const doorAlphaTexture = textureLoader.load("./src/textures/door/color.jpg");
-// const doorNormalTexture = textureLoader.load("./src/textures/door/normal.jpg");
-// const doorAmbientTexture = textureLoader.load("./src/textures/door/color.jpg");
-// const gradientTexture = textureLoader.load("./src/textures/gradients/3.jpg");
-// const metalnessTexture = textureLoader.load(
-//   "./src/textures/door/metalness.jpg",
-// );
-// const roughnessTexture = textureLoader.load(
-//   "./src/textures/door/roughness.jpg",
-// );
-// const doorAmbientOcclusionTexture = textureLoader.load(
-//   "./src/textures/door/color.jpg",
-// );
-
-// doorTexture.colorSpace = THREE.SRGBColorSpace;
-
-// // Create a group
-// const group = new THREE.Group();
-
-// Objects
-
-// const materiel = new THREE.MeshStandardMaterial();
-// materiel.metalness = 1;
-// materiel.roughness = 1;
-// materiel.map = doorTexture;
-// materiel.aoMap = doorAmbientOcclusionTexture;
-// materiel.aoMapIntensity = 1;
-// materiel.displacementMap = doorHeightTexture;
-// materiel.displacementScale = 0.1;
-// materiel.metalnessMap = metalnessTexture;
-// materiel.roughnesssMap = roughnessTexture;
-// materiel.normalMap = doorNormalTexture;
-// materiel.normalScale.set(0.5, 0.5);
-// materiel.transparent = true;
-// materiel.alphaMap = doorAlphaTexture;
-
-// MeshPhysicalMaterial
-// const materiel = new THREE.MeshPhysicalMaterial();
-// materiel.metalness = 0;
-// materiel.roughness = 0;
-// // materiel.map = doorTexture;
-// // materiel.aoMap = doorAmbientOcclusionTexture;
-// // materiel.aoMapIntensity = 1;
-// // materiel.displacementMap = doorHeightTexture;
-// // materiel.displacementScale = 0.1;
-// // materiel.metalnessMap = metalnessTexture;
-// // materiel.roughnesssMap = roughnessTexture;
-// // materiel.normalMap = doorNormalTexture;
-// // materiel.normalScale.set(0.5, 0.5);
-// // materiel.transparent = true;
-// // materiel.alphaMap = doorAlphaTexture;
-
-// // DEBUG UI
-// gui.add(materiel, "metalness", 0, 1, 0.01);
-// gui.add(materiel, "roughness", 0, 1, 0.01);
-
-// // Clearcot
-// materiel.clearcoat = 1;
-// materiel.clearcoatRoughness = 0;
-// gui.add(materiel, "clearcoat", 0, 1, 0.01);
-// gui.add(materiel, "clearcoatRoughness", 0, 1, 0.01);
-
-// // Sheen
-// materiel.sheen = 1;
-// materiel.sheenColor.set(1, 1, 1);
-// materiel.sheenRoughness = 0.25;
-
-// gui.add(materiel, "sheen", 0, 1, 0.01);
-// gui.add(materiel, "sheenRoughness", 0, 1, 0.01);
-// gui.addColor(materiel, "sheenColor");
-
-// Iridescence
-
-// materiel.iridescence = 1;
-// materiel.iridescenceIOR = 1;
-// materiel.iridescenceThicknessRange = [100, 800];
-
-// Transmission
-// materiel.transmission = 1;
-// materiel.ior = 1.5;
-// materiel.thickness = 0.5;
-
-// gui.add(materiel, "transmission", 0, 1, 0.001);
-// gui.add(materiel, "ior", 1, 10, 0.001);
-// gui.add(materiel, "thickness", 0, 1, 0.001);
-
-// const geo = new THREE.PlaneGeometry(1, 1, 100, 100);
-// const cube = new THREE.Mesh(geo, materiel);
-// cube.position.x = 1.5;
-
-// materiel.map = doorTexture;
-// mat.color = new THREE.Color("white");
-// mat.wireframe = true;
-// mat.side = THREE.DoubleSide;
-
-// 3D text
-
-// import font from "three/examples/fonts/helvetiker_regular.typeface.json";
-// const loader = new FontLoader();
-// loader.load("fonts/helvetiker_regular.typeface.json", function (font) {
-//   const geometry = new TextBufferGeometry("Hello World", {
-//     font: font,
-//     size: 5,
-//     height: 1,
-//     curveSegments: 12,
-//     bevelEnabled: true,
-//     bevelThickness: 0.1,
-//     bevelSize: 0.1,
-//     bevelSegments: 3,
-//   });
-// });
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
